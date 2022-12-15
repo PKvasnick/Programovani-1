e49ae8e5359ba345b67d7f2767842f66756408b4
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2022-12-15 14:58:41 +0100

    Ex10

--- code/Ex_6/sk.py
+++ code/Ex_6/sk.py
@@ -1,17 +1,5 @@
-from random import randint
+import sys
 
-seznam = [randint(1,100) for _ in range(10)]
-print(seznam)
-n = len(seznam)
-
-for i in range(n):
-    n_vymen = 0
-    for j in range(n-1):
-        if seznam[j] > seznam[j+1]:
-            seznam[j], seznam[j+1] = seznam[j+1], seznam[j]
-            n_vymen += 1
-    if n_vymen == 0:
-        break
-
-print(seznam)
-                              
+for line in sys.stdin:
+    print(line)
+    
