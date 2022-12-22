e5588e78916a61c35ef52f40e864d4c8572c5675
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2022-12-22 15:18:39 +0100

    Ex11

--- code/Ex_6/sk.py
+++ code/Ex_6/sk.py
@@ -1,5 +1,45 @@
-import sys
 
-for line in sys.stdin:
-    print(line)
+
+class Zvire:
+    """ Tŕída definující zvíře s daným jménoem a zvukem"""
+    
+    def __init__(self, jmeno, zvuk):
+        self.jmeno = jmeno
+        self.zvuk = zvuk
+
+    def slysi_na(self, jmeno):
+        return self.jmeno == jmeno
+
+    def ozvi_se(self):
+        print(f"{self.jmeno} říká {self.zvuk}")
+        return
+
+    def __str__(self):
+        return f"Zvire jmenem {self.jmeno} vydavajici zvuk {self.zvuk}"
+
+    def __repr__(self):
+        return f"Zvire(jmeno = {self.jmeno}, zvuk = {self.zvuk}"
+    
+    def __eq__(self, jine):
+        return (self.jmeno == jine.jmeno) and (self.zvuk == jine.zvuk)
+
+    def __bool__(self):
+        return bool(self.jmeno and self.zvuk)
+
+
+class Kocka(Zvire):
+
+    def __init__(self, jmeno, zvuk):
+        Zvire.__init__(self, jmeno, zvuk)
+        self.pocet_zivotu = 9
+
+    def slysi_na(self, jmeno):
+        return False
+
+
+
+    
+
+        
+
     
